2e601aae8be0bf7b0cb42b95a8eafee21693c7cb
Justine Mathew Zacharias <justinejmz@gmail.com>
2019-04-02 00:21:54 +0530

    Changed the code

--- file1.py
+++ file1.py
@@ -5,16 +5,15 @@ import hashlib
 import json
 md5dir={}
 perm={}
-for mydir,subdir,myfile in os.walk('/bin'):
-    for myfile in myfile:
-        path=os.path.join(mydir,myfile)
+for curdir,subdir,curfile in os.walk('/bin'):
+    for curfile in curfile:
+        path=os.path.join(curdir,curfile)
         checksum = getmd5.getmd5(path)
         md5dir[path]=checksum
         info = os.stat(path)
         per=oct(info.st_mode)[-3:]
         perm[path]=per
-
+        md5dir[path]=[checksum,per]
         json.dump(md5dir,open('files.json','w'),indent=2)
-        json.dump(perm,open('permission.json','w'),indent=2)
 
 
--- file2.py
+++ file2.py
@@ -3,27 +3,53 @@ import sys
 import getmd5
 import hashlib
 import json
-md5cha={}
-md5ch={}
-md5noch={}
+
+#Dictinory to store old values
+md5old={}
+
+#Dictinory to store new values
+md5new={}
+
+#Dictinary which shows file has not changed
+md5nochange={}
+
+#Dictinary which shows files have changed
 md5changed={}
+
+out='No changes in the files'
 json_file="/root/cyber/files.json"
-for mydir,subdir,myfile in os.walk('/bin'):
-    for myfile in myfile:
-        path=os.path.join(mydir,myfile)
+
+#Takes the file checksum
+for curdir,subdir,curfile in os.walk('/bin'):
+    for curfile in curfile:
+        path=os.path.join(curdir,curfile)
         checksum = getmd5.getmd5(path)
-        md5cha[path]=checksum
+        info = os.stat(path)
+        per=oct(info.st_mode)[-3:]
+        md5new[path]=[checksum,per]
        # json.dump(md5dir,open('files.json','w'),indent=2)
-md5ch=json.load(open(json_file))
+md5old=json.load(open(json_file))
 change="/root/cyber/chaged.txt"
 files=open(change,'w')
-for i in md5cha.keys():
-    for j in md5ch.keys():
-        if j==i:
-            if md5cha[i] == md5ch[j]:
-                md5noch[i]=md5cha[i]
-            else:
-                md5changed[i]=md5cha[i]
+
+#compairing the values
+for oldkey,oldvalue in md5old.items():
+    oldv1,oldv2=oldvalue
+    for newkey,newvalue in md5new.items():
+        newv1,newv2=newvalue
+        if oldkey==newkey:
+            if oldv1!=newv1: 
+                out="This  contents are changed"
+                md5changed[oldkey]=out
+            if oldv2!=newv2:
+                out="This  permissions are changed"
+                md5changed[oldkey]=out
+            if oldv1!=newv1 and oldv2!=newv2:
+                out="This files permissios and contents are changed"
+                md5changed[oldkey]=out
+           # if oldv1!=newv1 and oldv2==newv2:
+            #    out="This files contents are changed"
+   # md5changed[oldkey]=out
 
 json.dump(md5changed,open('changedfiles.json','w'),indent=2)
 
